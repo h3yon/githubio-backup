7f83a48dc413d6e182a7aad39e7223813ecc43db
h3yon <khykhy1006@gmail.com>
2021-09-24 17:44:19 +0900

    feat: you don't know js chapter1,2

---
+++ _posts/2021-09-24-you-dont-know-js.markdown
@@ -0,0 +1,545 @@
+---
+title: "You dont know js 챕터1,2 정복하기"
+except: "you dont knnow js!"
+
+toc: true
+toc_sticky: true
+
+categories:
+  - Javascript
+tags:
+  - Javascript
+---
+
+### You dont know js 1st Edition 챕터1, 2 정복하기
+
+You dont know 책을 접해보게 되었는데,  
+뭔가 탄탄해보여서 읽어보기로 결정했다!
+
+(기본 지식을 가졌다고 가정하고 정리해보겠습니다!)
+
+#### Chapter 1: Into Programming
+
+1. Input
+
+```javascript
+let age = prompt("나이를 입력해주세요");
+console.log(age);
+```
+
+2. Converting Between Types
+
+```javascript
+var a = "42";
+var b = Number(a);
+
+console.log(a); // "42"
+console.log(b); // 42
+```
+
+3. Comments
+
+comment는 what이 아닌, 이유를 설명해야 한다
+
+```javascript
+// 한줄짜리 주석
+/*
+ * 주석이 multiline일 때
+ * 주석주석
+ */
+```
+
+4. 소수점 몇째자리까지
+
+toFixed()는 String으로 만든다.
+
+```javascript
+var amount = 215.9784;
+console.log(amount.toFixed(2)); // "215.98"
+```
+
+5. Scope
+
+아래처럼 각 함수마다 그들만의 scope를 가지기 때문에  
+각각 다른 결과가 나옴을 알 수 있다.
+
+```javascript
+function one() {
+  // this `a` only belongs to the `one()` function
+  var a = 1;
+  console.log(a);
+}
+
+function two() {
+  // this `a` only belongs to the `two()` function
+  var a = 2;
+  console.log(a);
+}
+
+one(); // 1
+two(); // 2
+```
+
+아래 예제에서는 다른 scope 안에 중첩될 수 있음을 보여준다.
+여기서 inner함수는 a, b에 대한 접근이 가능하지만,
+outer함수는 a에 대한 접근만 가능함을 알 수 있다.
+
+그런데, const 타입인 TAX_RATE는 함수 밖에 있음에도
+inner함수에서 사용 가능함을 알 수 있다.
+
+```javascript
+const TAX_RATE = 0.08;
+
+function outer() {
+  var a = 1;
+
+  function inner() {
+    var b = 2;
+    // we can access both `a` and `b` here
+    console.log(a + b); // 3
+    console.log(a * TAX_RATE); // 0.24
+  }
+  inner();
+
+  // we can only access `a` here
+  console.log(a); // 1
+}
+
+outer();
+```
+
+#### Chapter 2: Into JavaScript
+
+1. Values & Types
+
+```
+
+[typeof 변수명]하면 나올 수 있는 부분
+
+- string
+- number
+- boolean
+- null and undefined
+- object
+- symbol (new to ES6)
+```
+
+2. Objects
+
+```javascript
+var obj = {
+  a: "hello world",
+  b: 42,
+  c: true,
+};
+
+// Dot 노테이션: 짧고 읽기 쉬워서 가능한 많이 사용되는 부분
+
+obj.a; // "hello world"
+obj.b; // 42
+obj.c; // true
+
+// Bracket 노테이션: property/key로 접근하고자 할 때 도움된다
+
+obj["a"]; // "hello world"
+obj["b"]; // 42
+obj["c"]; // true
+```
+
+3. Array
+
+배열은 거의 다 알고 있을 것 같은데,  
+array 타입이 object인 걸 한번 더 짚고 넘어가면 될 것 같다!
+
+```javascript
+var arr = [1, 2, "hi"];
+
+arr[0]; // 1
+arr[1]; // 2
+arr[2]; // 'hi'
+
+typeof arr; // "object"
+```
+
+4. function
+
+함수 같은 경우, typeof foo();를 하면  
+리턴값의 타입인 number가 나오는 것이 새로웠다!
+
+```javascript
+function foo() {
+  return 42;
+}
+
+foo.bar = "hello world";
+
+typeof foo; // "function"
+typeof foo(); // "number"
+typeof foo.bar; // "string"
+```
+
+5. 내장 함수 짚고 넘어가기
+
+```javascript
+var a = "hello world";
+var b = 3.14159;
+
+a.length; // 11
+a.toUpperCase(); // "HELLO WORLD"
+b.toFixed(4); // "3.1416"
+```
+
+6. Truthy & Falsy
+
+false로 인식될 수 있는 부분은  
+아래와 같다
+
+```javascript
+- "" (empty string)
+- 0, -0, NaN (invalid number)
+- null, undefined
+- false
+```
+
+true로 인식될 수 있는 부분은  
+아래와 같다.  
+빈 배열, 빈 Object도 true로 인식된다.
+
+```javascript
+- "hello"
+- 42
+- true
+- [ ], [ 1, "2", 3 ] (arrays)
+- { }, { a: 42 } (objects)
+- function foo() { .. } (functions)
+```
+
+7. Equality
+
+Equality 비교 연산자로는 '==', '===', '!=', '!==', '!'가 있다.
+
+```javascript
+var a = "42";
+var b = 42;
+
+a == b; // true
+a === b; // false(타입까지 비교)
+```
+
+그런데, non-primitive 타입(즉, function, array, object)의 경우는  
+위의 부분과 좀 다르다.
+
+```javascript
+var a = [1, 2, 3];
+var b = [1, 2, 3];
+var c = "1,2,3";
+
+a == c; // true
+b == c; // true
+a == b; // false
+```
+
+references가 매칭되는지를 확인하기 때문이라고 한다.
+위에서 배열과 string이 같은 부분은,
+array 요소가 쉼표로 묶이고 최종적으로 String화 되는 부분과 같다는 걸 알 수 있다.
+
+8. Function Scopes(함수 기반 스코프)
+
+- 호이스팅
+  : 호이스팅은 둘러싸는 범위의 위로 이동될 때를 말한다.
+  var의 경우만 나타나있는데, let/const는 TDZ에 의해서 초기화 전에 액세스하려고 하면 에러가 발생한다.
+  let/const의 호이스팅은 나중에 알면 좋을 것 같다.
+
+```javascript
+var a = 2;
+
+foo();
+// declaration 전에 사용 가능됨을 알 수 있는데,
+// 호이스팅되었음을 알 수 있다.
+
+function foo() {
+  a = 3;
+  console.log(a); // 3
+
+  var a;
+  // foo() scope위로
+  // declaration이 호이스팅되었음을 알 수 있다.
+}
+
+console.log(a); // 2
+```
+
+함수 기반 scope 예제를 보자.
+var a는 함수 안에만 있기 때문에
+함수 밖에서는 값이 안 나타남을 알 수 있다.
+
+```javascript
+function foo() {
+  var a = 1; // `a` not formally declared
+}
+
+foo();
+a; // []
+```
+
+9. 블록 기반 scope
+
+var 대신 let을 사용하면 어떨까?  
+let은 아래 예제를 보면 while{}안에 있다.  
+그래서 let c는 가장 가까운 {}문, whild문에서만 사용할 수 있음을 볼 수 있다.
+
+그래서 let, const는 블록기반 scope이다
+
+```javascript
+function foo() {
+  var a = 1;
+
+  if (a >= 1) {
+    let b = 2;
+
+    while (b < 5) {
+      let c = b * 2; // 4, 6, 8
+      b++; // 3, 4, 5 -> 멈춤
+
+      console.log(a + c);
+    }
+  }
+}
+
+foo();
+// 5 7 9
+```
+
+10. Strict Mode
+
+ES5에서는 "strict mode"가 추가되었는데,  
+rule을 더 타이트하게 함을 알 수 있다.
+
+더 안전하고, 가이드라인에 더 적합하고,
+더 안전한 방식을 고수하게 하며 최적화에 도움을 주므로  
+많이 사용되어야 함을 알 수 있다.
+
+```javascript
+function foo() {
+  "use strict"; // turn on strict mode
+  a = 1; // `var` missing, ReferenceError
+}
+
+foo();
+```
+
+11. Functions As Values
+
+아래와 같은 구조를 많이 볼 수 있다.
+
+```javascript
+var foo = function () {
+  // 익명함수..
+};
+
+var x = function bar() {
+  // 명명된 함수 표현식..
+};
+```
+
+function 결과값을 변수에 할당한다.
+
+12. 즉시 호출된 함수 표현식 (IIFEs)
+
+```javascript
+(function IIFE() {
+  console.log("Hello!");
+})(); // "Hello!"
+
+var a = 42;
+
+(function IIFE() {
+  var a = 10;
+  console.log(a); // 10
+})();
+
+console.log(a); // 42
+```
+
+})();줄은 바로 앞에 참조된 함수 표현식을 실제로 실행함을 알 수 있다.
+() 안에서 함수가 진행되기 때문에 주변 코드에 영향이 가지 않는다.
+
+13. \*Closure(클로저)
+
+클로저는 진짜 중요한 개념이다.  
+이 책에서는 깊게 파고들진 않지만 scope&Closure를 언급한다고 한다.
+
+closure은 "remember"로 생각할 수 있다.
+
+```javascript
+function makeAdder(x) {
+  // x는 내부 변수이고
+  // inner 함수인 add는 x를 사용하기 때문에
+  // 클로저를 가진다.
+  function add(y) {
+    return y + x;
+  }
+  return add;
+}
+
+// add(..) inner 함수에 대한 reference를 가진다.
+var plusOne = makeAdder(1); // y -> 1
+var plusTen = makeAdder(10); // y -> 10
+
+plusOne(3); // 4  <-- 1 + 3
+plusOne(41); // 42 <-- 1 + 41
+
+plusTen(13); // 23 <-- 10 + 13
+```
+
+14. Module
+
+클로저의 일반적인 사용은 모듈 패턴이다.  
+모듈은 외부로부터 숨겨진 private implementation details(변수, 함수) 뿐만 아니라  
+외부로부터 접근 가능한 Public API도 정의하도록 한다.
+
+```javascript
+function User() {
+  var username, password;
+  function doLogin(user, pw) {
+    username = user;
+    password = pw;
+    // do the rest of the login work
+  }
+
+  var publicAPI = {
+    login: doLogin,
+  };
+  return publicAPI;
+}
+
+// create a `User` module instance
+var fred = User();
+fred.login("fred", "12Battery34!");
+```
+
+User가 클래스가 아닌 함수로써 사용됨을 알 수 있다.  
+`User`모듈의 인스턴스를 생성함을 알 수 있고,  
+inner 변수/함수 각각을 새로 copy함을 알 수 있다.
+
+내부 함수 doLogin은 username과 password에 대한 클로저를 가진다.  
+그래서 User()가 running이 끝난 후에도 2개 변수에 대한 access를 지속한다.
+
+`publicAPI`는 object로, login: doLogin 형태를 리턴한다.
+그래서 User().login으로 User()함수는 실행을 끝내고  
+username, password는 날아가지만,  
+doLogin에는 클로저를 가지고 있기 때문에 username, password가 살아있다.  
+그래서 계속 내부 변수에 대해 접근할 수 있다.
+
+15. this Identifier
+
+this는 객체 지향 패턴과 관련 있어 보이지만,  
+자바스크립트에서는 다른 메커니즘을 가진다고 한다.
+
+만약 함수가 this reference를 내부에 가진다면  
+이 this는 object를 주로 가리킨다고 한다.  
+하지만 object가 가리키는 것은 함수가 어떻게 호출되었는지에 달려 있다.
+
+가장 큰 오해로, this가 함수 그자체를 refer하지 않는다는 것을 깨닫는 것이 중요하다고 한다.
+
+```javascript
+function foo() {
+  console.log(this.bar);
+}
+
+var bar = "global";
+
+var obj1 = {
+  bar: "obj1",
+  foo: foo,
+};
+
+var obj2 = {
+  bar: "obj2",
+};
+
+// --------
+
+foo(); // "global" - 글로벌 object의 bar
+obj1.foo(); // "obj1" - obj1의 bar
+foo.call(obj2); // "obj2" - 이 예제는 this를 obj2로 세팅한다.
+new foo(); // undefined - 새로운 빈 object를 this로 삼기 때문에 undefined이다.
+```
+
+16. Prototypes
+
+자바스크립트에서 프로토타입은 복잡하다.  
+객체 속성을 참조할 때 해당 속성이 없으면  
+자동으로 해당 객체의 내부 프로토타입 참조를 사용하여 속성을 찾을 다른 객체를 찾는다.  
+속성이 누락된 경우 이를 거의 fallback으로 생각한다.
+
+```javascript
+var foo = {
+  a: 42,
+};
+
+// create `bar` and link it to `foo`
+var bar = Object.create(foo);
+
+bar.b = "hello world";
+
+bar.b; // "hello world"
+bar.a; // 42 <-- delegated to `foo`
+```
+
+17. newer JavaScript: polyfilling and transpiling
+
+자바스크립트를 구형 브라우저에 가져오는데 사용 할 수 있는  
+`polyfilling`과 `transpiling`이 있다.
+
+- polyfilling: 이전 JS 환경에서 실행할 수 있는 코드 조각
+
+- transpiling: 최신 코드를 이전 코드로 변환하는 도구로,
+  transfer + compile이 합쳐진 단어
+
+```javascript
+// newer
+function foo(a = 2) {
+  console.log(a);
+}
+
+// older
+function foo() {
+  var a = arguments[0] !== void 0 ? arguments[0] : 2;
+  console.log(a);
+}
+```
+
+transpiler는 JS 개발 ecosystem 및 프로세스의 표준 부분으로 간주되어야 한다.  
+JS는 이전보다 훨씬 더 빠르게 계속 발전할 것이므로  
+몇 개월마다 새로운 구문과 새로운 기능이 추가될 것이라고 한다.
+
+기본적으로 트랜스파일러를 사용하는 경우  
+오늘날의 브라우저가 단계적으로 사라질 때까지 몇 년을 기다리지 않고  
+유용하다고 생각될 때마다 항상 새 구문으로 전환할 수 있다.
+
+이러한 예로는 Babel(ES6+ -> ES5로 변환),  
+Traceur(ES6,7 -> ES5)로 변환한다.
+
+18. Non-JavaScript
+
+Non-JavaScript로는 DOM API가 있다.
+
+```javascript
+var el = document.getElementById("foo");
+```
+
+또 입/출력이 있다.
+
+#### 마지막 소감
+
+와 정말 챕터 1,2만 하는데도 힘든 것 같다.  
+영어라서 그런가..  
+호이스팅과 클로저는 좀더 알아봐야 될 것 같지만,  
+이 책에서는  
+호이스팅 - 속해있는{}의 위로 끌어올리는 것
+클로저 - remember!
+
+다음에 더 읽어야겠돠..
+
+출처: https://github.com/getify/You-Dont-Know-JS/tree/1st-ed
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,157 @@
 var store = [{
         "title": "JAVA#1 ORM과 JPA, REST API",
         "excerpt":"ORM(Object-relational mapping) 이란 Object-Relational Mapping (객체 관계 매핑) 객체는 객체대로 설계하고, 관계형 데이터베이스는 관계형 데이터베이스대로 설계한다. ORM 프레임워크가 중간에서 매핑해준다. 대중적인 언어에는 대부분 ORM 기술이 존재한다. ORM은 객체와 RDB 두 기둥 위에 있는 기술 이다. JPA(Java Persistence API)란? EJB: 과거의 자바 표준(Entity Bean) 과거의 ORM → 코드가 매우 지저분하다. API...","categories": ["Java"],
-        "tags": ["Java","ORM","JPA"],
+        "tags": ["개념"],
         "url": "/java/java-1_orm-jpa-rest-api/",
+        "teaser": null
+      },{
+        "title": "JAVA#2 Lambok",
+        "excerpt":"Lombok   Lombok : Getter/Setter 그리고 생성자들을 어노테이션으로 간편하게 사용할 수 있게 해주는 플러그인   setter 메서드가 필요 없는 필드에 대해서도 setter 메서드를 강제로 생성하게 되므로, 필드 값이 변경될 위험이 생김   -&gt; 나중에 리팩토링 대상이 될 수 있다     출처: http://wonit.tistory.com/38?category=738059  ","categories": ["Java"],
+        "tags": ["Java"],
+        "url": "/java/java-2-lombok/",
+        "teaser": null
+      },{
+        "title": "어노테이션 정리",
+        "excerpt":"Slf4j : 1. Slf4j: redirect logging https://gmlwjd9405.github.io/2019/01/04/logging-with-slf4j.html → SLF4J Bridging Modules: 다른 로깅 API로의 로거 호출을 SLF4J 인터페이스로 연결(redirect)하여 SLF4J API가 대신 처리할 수 있도록 하는 일종의 어댑터 역할을 하는 라이브러리 → SLF4J API: 로깅에 대한 추상 레이어를 제공, 사용자가 이 인터페이스를 통해 logging 코드를 작성 → SLF4J 특징: 배포시...","categories": ["Java"],
+        "tags": ["Java"],
+        "url": "/java/java-3-annotation/",
+        "teaser": null
+      },{
+        "title": "Swagger 적용",
+        "excerpt":"[“java”] Swagger 적용하기 build.gradle compileOnly group: 'io.springfox', name: 'springfox-swagger2', version: '2.9.2' compileOnly group: 'io.springfox', name: 'springfox-swagger-ui', version: '2.9.2' swagger 파일 class 밖에 @Configuration, @EnableSwagger2 class 안에 @Bean @Bean @Configuration으로 정의된 클래스는 @Bean으로 정의된 메소드들을 포함 .consume(), .produces(): 각각 Request Content-Type, Response Content-Type에 대한 설정(선택) .apiinfo(): Swagger API 문서에 대한 설명을...","categories": ["Java"],
+        "tags": ["Swagger"],
+        "url": "/java/java-4-swagger/",
+        "teaser": null
+      },{
+        "title": "[Springboot] kakao api 구현",
+        "excerpt":"스프링부트로 kakao API 구현해보기 https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api 일단, 위의 사이트에서 kakao API의 흐름을 살펴보자. 보면 인가코드를 우선 받아야 함을 알 수 있다. 일단 그러면 어플리케이션 키를 받아야 한다. 위의 이미지처럼 상단의 ‘내 애플리케이션’ 버튼을 누른다. 아무 애플리케이션이나 만들고, 원하는 앱키를 복사해놓는다. 또 옆에 플랫폼으로 들어가, 원하는 플랫폼에 대한 플랫폼을 등록한다. 위처럼 등록해도...","categories": ["Java"],
+        "tags": ["소셜로그인"],
+        "url": "/java/java-5-kakao-api/",
+        "teaser": null
+      },{
+        "title": "정보처리기사 실기 벼락치기",
+        "excerpt":"정보처리기사 실기 벼락치기 시도해보기 요구사항 확인 현행 시스템 파악: 현재 개발하고자 하는 시스템의 개발 범위를 설정하기 위해 구성과 기능, 연계 정보, 소프트웨어, 하드웨어, 네트워크 구성을 파악하는 과정 현행시스템 파악 절차 현행 시스템 구성 파악: 기간 업무, 지원 업무 현행 시스템 기능 파악: 제공 기능 파악, 계층형 표시 인터페이스 현황 평가:...","categories": ["IT"],
+        "tags": ["정보처리기사"],
+        "url": "/it/%EC%A0%95%EB%B3%B4%EC%B2%98%EB%A6%AC%EA%B8%B0%EC%82%AC-%EC%8B%A4%EA%B8%B0-%EB%B2%BC%EB%9D%BD%EC%B9%98%EA%B8%B0-%EC%8B%9C%EB%8F%84/",
+        "teaser": null
+      },{
+        "title": "MySQL User 추가 및 권한 부여하기",
+        "excerpt":"MySQL User 추가 및 권한 부여하기 mysql 사용자 추가하기 사용자 추가(비밀번호 X) $ create user 사용자; 사용자 추가 및 패스워드 설정 $ create user 사용자@localhost identified by 비밀번호; 사용자 추가 및 패스워드 설정부터 외부에서의 접근 허용 $ create user 사용자@’%’ identified by 비밀번호; 사용자 삭제1 $ drop user 사용자; 사용자...","categories": ["MySQL"],
+        "tags": ["mysql"],
+        "url": "/mysql/mysql-user/",
+        "teaser": null
+      },{
+        "title": "Javascript 기초지식",
+        "excerpt":"출처: https://velog.io/@dongha1992/javascript-%EB%82%B4%EB%B6%80%ED%95%A8%EC%88%98-%EC%99%B8%EB%B6%80%ED%95%A8%EC%88%98 자바스크립트 기초 지식 쌓기 아래의 코드가 있다고 가정하자. function out(){ const name = \"aaa\" return function(){ console.log(name); } } const in = out() in() 함수 안에 함수가 있을 때 응집성이 높아진다. 위의 코드를 보면 out()은 외부함수, in()은 내부함수를 말한다. in()함수에서 name에 대한 로그를 찍는데, name은 out()에만 있기 대문에...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/javascript/",
+        "teaser": null
+      },{
+        "title": "Javascript 변수 var, let, const & TDZ(Temporal Dead Zone)",
+        "excerpt":"출처 자바스크립트 변수 선언 방식 차이: var/let/const TDZ(Temporal Dead Zone)이란? Hoisting and TDZ Javascript 변수 var, let, const &amp; TDZ(Temporal Dead Zone) 오늘은 기초적인 var, let, const에 대한 차이를 알아보았다 일단 변수 선언의 3단계는 선언 -&gt; 초기화 -&gt; 할당 과정으로 이루어져 있다. 선언: 변수를 실행 context의 변수 객체에 등록하는 단계....","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/javascript1/",
+        "teaser": null
+      },{
+        "title": "Javascript 배우기",
+        "excerpt":"Javascript 배우기 Javascript는 웹 브라우저에서 실행된다.(클라이언트 측) JSP, Servlet, ASP는 웹서버에서 해석되고 실행되어 결과만 HTML로 변환되어 웹 브라우저에 보여준다.(서버측) 웹서버에서 실행되기 때문에 즉각적인 반응을 하지 못하고 시간이 오래 걸린다. 프로토타입 기반 객체 지향 자바스크립트는 프로토타입 기반 객체지향 언어로, 자바처럼 클래스로부터 내용을 상속 받아서 객체 만드는 것이 아니라, 그냥 간단하게 객체를...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/javascript2/",
+        "teaser": null
+      },{
+        "title": "Javascript - forEach, map, filter, reduce 총정리",
+        "excerpt":"Javascript - forEach, map, filter, reduce 총정리 참고 자료: ES6의 map, filter, reduce 정리 reduce 참고 링크 forEach 가장 기본적인 Loop 메소드 const arr = [\"a\", \"bb\", \"ccc\", \"ddddd\"]; var arr2 = []; arr.forEach(function (str) { arr2.push(str.length); }); console.log(arr2); // [1, 2, 3, 5]] forEach문과 아래 map() 기능이 완전 똑같음을...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/map-filter-reduce/",
+        "teaser": null
+      },{
+        "title": "Nodejs 스레드",
+        "excerpt":"Nodejs 스레드 자바스크립트 코드가 실행될 때 보다, IO 이벤트가 발생할 때 까지 기다리는 시간이 훨씬 더 많다. node index.js 한 후, lsof -i:[사용하는 포트 번호] ex) lsof -i:3000 하면 해당 프로세스의 pId를 확인할 수 있다. ps -M [pId] -&gt; 스레드의 개수를 확인 가능 USER PID TT %CPU STAT PRI STIME...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/nodejs-thread/",
+        "teaser": null
+      },{
+        "title": "멀티스레드",
+        "excerpt":"[“it”] 출처: [병렬프로그래밍] 프로세스, 스레드 개념 [병렬프로그래밍] 스레드 이해하기 스레드 리마인드 프로세스는 실행중인 프로그램이며, 하나의 실행 흐름을 스레드라고 한다. 하나의 프로세스 안에 스레드 여러개 있는 것이 멀티 스레드. 게임/네트워크 프로그래밍에서 멀티 스레드 많이 사용. (캐릭터를 움직이게 하거나, 데이터를 기다릴 때, 흐름 담당할 때 사용) 멀티 프로그래밍 여러 개의 프로그램들이 단일...","categories": ["IT"],
+        "tags": ["IT"],
+        "url": "/it/multi-thread/",
+        "teaser": null
+      },{
+        "title": "스레드",
+        "excerpt":"[“it”] 출처: [병렬프로그래밍] 자바 병렬 프로그래밍 소개 스레드 사용 안전성 위해 요소 UnsafeSequence는 비표준 어노테이션인 @NotThreadSafe를 사용.(@ThreadSafe와 @Immutable도 존재) @Immutable은 해당 클래스가 불변 클래스임을 나타내기 때문에 ThreadSafe 만약 @ThreadSafe라고 표시하면 클래스를 사용하는 사람은 멀티스레드 환경에서 문제가 없다는 점을 명확히 알 수 있고, 스레드 안전성이 계속 보장돼야 한다는 점에 주의할 수...","categories": ["IT"],
+        "tags": ["IT"],
+        "url": "/it/thread/",
+        "teaser": null
+      },{
+        "title": "Nodejs에서의 멀티스레드, 멀티코어프로세스",
+        "excerpt":"Nodejs에서의 멀티스레드, 멀티코어프로세스 출처: How to run many parallel HTTP requests using Node.js ? nodejs는 싱글 스레드로, 10초가 소요되는 요청 A가 포함되는 경우, 다음 요청을 처리 하기 위해 10초를 기다리는 건 X! NodeJS 이벤트 루프가 단일 스레드일 뿐이기 때문. 여러 클라이언트 요청을 수신하여 EventQueue에 배치. 이벤트루프 NodeJS는 이벤트 중심 아키텍처의...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/nodejs-requests/",
+        "teaser": null
+      },{
+        "title": "멀티 프로세스 & IPC & 세마포어,뮤텍스",
+        "excerpt":"멀티 프로세스 &amp; IPC &amp; 세마포어,뮤텍스 출처: multi process vs multi thread 컨텍스트 스위칭 한 task가 끝날 때까지 기다리는 게 아니라 여러 작업을 번갈아가며 동시에 처리한다. 인터럽트 발생 시 프로세스 상태를 PCB에 저장하고 새로운 프로세스 상태를 레지스터에 저장한다. 이때 CPU는 아무런 일을 하지 않기 때문에 잦은 컨텍스트 스위칭은 성능저하를 야기한다....","categories": ["IT"],
+        "tags": ["IT"],
+        "url": "/it/%EC%9A%B4%EC%98%81%EC%B2%B4%EC%A0%9C/",
+        "teaser": null
+      },{
+        "title": "Node.js의 ORM, sequeliz",
+        "excerpt":"Node.js의 ORM, sequelize 다들 ORM에 대해서 들어봤을 것 같다. ORM이란? Object-Relational Mapping으로 객체와 관계형 데이터베이스의 데이터를 매핑(연결)시켜주는 것 ORM은 객체-관계 매핑의 줄임말이다. 객체 개념을 구현한 class와 RDB(Relational DB)에서 쓰이는 테이블을 자동으로 매핑하는 것이다. ORM 프레임워크의 경우, JPA/Hibernate JPA(Java Persistence API)는 자바의 ORM 기술 표준으로 인터페이스의 모음이다. 이러한 JPA 표준 명세를...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/sequelize/",
+        "teaser": null
+      },{
+        "title": "스프링부트 Naver Login 구현하기",
+        "excerpt":"스프링부트 Naver Login 구현하기 출처: 이동욱 님의 스프링 부트와 AWS로 혼자 구현하는 웹 서비스 스프링 부트와 OAuth2 Naver) 사용한 방식 저번에 카카오 로그인을 구현하였을 때는 restTemplate을 사용했었다. 그런데 이번에는 spring-security-oauth2 부분을 알아보고 사용해보고자 했다. 라이브러리 중에 spring-security-auth2-autoconfigure이 있는데, 스프링부트2에서 기존 설정을 그대로 사용할 수 있어 많은 개발자가 이 방식을 사용했다고...","categories": ["Java"],
+        "tags": ["Java","소셜로그인"],
+        "url": "/java/spring-naver-login/",
+        "teaser": null
+      },{
+        "title": "[node.js]http2 적용과 발생한 에러",
+        "excerpt":"[node.js]http2 적용과 발생한 에러 리액트 웹페이지에 적용시킬 백엔드 서버를 구동하는 코드를 원래는 아래처럼 작성해놓았었다. # index.js ... express().listen(PORT); console.info(`${NODE_ENV} - API Server Start At Port ${PORT}`); http2의 장점을 알고, 해당 코드를 http2로 변경해주었다. const express = require(\"./config/express\"); const { readFileSync } = require(\"fs\"); const http2 = require(\"http2\"); const { NODE_ENV,...","categories": ["Javascript"],
+        "tags": ["Javascript","http2"],
+        "url": "/javascript/http2/",
+        "teaser": null
+      },{
+        "title": "[Java]자바의 정석 벼락치기",
+        "excerpt":"자바의 정석 벼락치기 정보처리기사 실기 2일인지 3일인지 잘 기억은 안 나는데 이번에 이틀의 전사? 3일의 전사 기념으로 자바의 정석도 벼락치기 해도 될 것 같다! ch2 변수 명명규칙 대소문자 구분. 길이에 제한 X 예약어 사용해선 X 예약어란? 자바에서 자체적으로 사용하는 단어 - true는 예약어. 하지만 True는 가능 숫자로 시작해선 X 특수문자는...","categories": ["Java"],
+        "tags": ["Java"],
+        "url": "/java/JAVA/",
+        "teaser": null
+      },{
+        "title": "[Github]올라간 파일/디렉토리 삭제하기",
+        "excerpt":"올라간 파일/디렉토리 삭제하기 .gitignore에 안 올릴 파일을 명시하고 올렸어야 했는데, 까먹고 그냥 올려버렸다. 로컬 디렉토리와 git 저장소에서 모두 삭제 $ git rm a.txt $ git commit -m \"chore: delete a.txt\" $ git push git에서만 삭제 로컬 디렉토리에서는 유지해야 할 때 $ git rm --cached a.txt $ git commit -m \"chore:...","categories": ["IT"],
+        "tags": ["Github"],
+        "url": "/it/github-delete/",
+        "teaser": null
+      },{
+        "title": "[Github]커밋 되돌리기",
+        "excerpt":"커밋 되돌리기 일단 로그를 먼저 봐야 한다. 아래 명령어를 쳐서 되돌아갈 부분을 보자. % git log --oneline 4170f95 (HEAD -&gt; master, origin/master) Delete replication/slave/data directory dd16435 Delete replication/master/data directory b2e987b .gitignore 8a550be .gitignore 4ce5b09 feat: replication 4f40e6f fin: server 나는 b2e987b로 돌아가면 된다. 여러가지 옵션이 있는데, 아래 부분을 보고 하나...","categories": ["IT"],
+        "tags": ["Github"],
+        "url": "/it/revert-commit/",
+        "teaser": null
+      },{
+        "title": "[Javascript]자바스크립트 Again",
+        "excerpt":"자바스크립트 복습 출처: 제주코딩베이스캠프 중간에 추가하기 아래처럼 만드려면? var arr = [200, 100, 300]; [200, 100, 10000, 300]; 답: arr.splice(2,0,10000); 다음 출력 값으로 올바른 것은? var arr = [100, 200, 300]; console.log(typeof arr); 답: object 객체의 키 이름 중복 var d = { \"height\": 180, \"weight\": 78, \"weight\": 84, };...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/javascript-again/",
+        "teaser": null
+      },{
+        "title": "[Javascript]자바스크립트 Again2",
+        "excerpt":"자바스크립트 복습2 출처: 제주코딩베이스캠프 merge sort 병합정렬(merge sort)은 대표적인 정렬 알고리즘 중 하나로 다음과 같이 동작합니다. 리스트의 길이가 0 또는 1이면 이미 정렬된 것으로 본다. 그렇지 않은 경우에는 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눈다. 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다. 두 부분 리스트를 다시...","categories": ["Javascript"],
+        "tags": ["Javascript"],
+        "url": "/javascript/javascript-again2/",
+        "teaser": null
+      },{
+        "title": "[Jenkins]AWS EC2 Ubuntu에 Jenkins 연동하기",
+        "excerpt":"1. AWS EC2 Ubuntu에 Jenkins를 연동해보자 관련 패키지 설치 Using Ubuntu curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - sudo apt-get install -y nodejs 출처: https://github.com/nodesource/distributions/blob/master/README.md ubuntu@ip-172-31-35-209:~/jenkins-test$ sudo apt install libssl1.0-dev Reading package lists… Done Building dependency tree Reading state information… Done The following additional packages will be installed: libssl1.0.0...","categories": ["Jenkins"],
+        "tags": ["Jenkins"],
+        "url": "/jenkins/jenkins-ec2-master/",
+        "teaser": null
+      },{
+        "title": "[Jenkins]AWS EC2 Ubuntu에 Jenkins 연동하기",
+        "excerpt":"1. AWS EC2 Ubuntu에 Jenkins를 연동해보자 관련 패키지 설치 Jenkins 설치를 위해 Repository key 추가 $ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add - 서버의 sources.list에 Jenkins 패키지 저장소를 추가 $ sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ &gt; \\ /etc/apt/sources.list.d/jenkins.list' 패키지 업데이트 및 Jenkins 설치 $ sudo...","categories": ["Jenkins"],
+        "tags": ["Jenkins"],
+        "url": "/jenkins/jenkins-ec2/",
         "teaser": null
       }]
